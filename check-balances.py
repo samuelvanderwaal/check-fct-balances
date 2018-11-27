52df818c2d47267a3d88c413f40ecabef6b26813
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2018-11-27 13:08:02 -0900

    v0.1, supports adding your own addresses to config file

---
+++ check-balances.py
@@ -0,0 +1,40 @@
+import json
+from factom import Factomd, FactomWalletd
+from coinmarketcap import Market
+
+FACTOSHI_CONV = 1.0E8
+total_balance = 0
+
+f = open("config.json")
+
+config = json.load(f)
+
+host = config["host"]
+
+factomd = Factomd(
+    host=host
+    )
+walletd = FactomWalletd()
+
+cmc = Market()
+listings = cmc.listings()
+
+for currency in listings["data"]:
+    if currency["symbol"] == "FCT":
+        fct_id = currency["id"]
+
+price = cmc.ticker(fct_id, convert="USD")["data"]["quotes"]["USD"]["price"]
+
+
+for name, addr in config["addresses"].items():
+    spacing_offset = 27 - len(name)
+    balance = int(factomd.factoid_balance(addr)["balance"])/FACTOSHI_CONV
+    print("{} balance:             {:>{}}".format(name, balance, spacing_offset))
+    total_balance += balance
+
+print("-------------------------------------------------")
+print("Total Factoids:              {:>20}".format(total_balance))
+print("-------------------------------------------------")
+print("Price:                  {:>20}${:,.2f}".format(" ", price))
+print("-------------------------------------------------")
+print("Current FCT Value: {:>20}${:,.2f}".format(" ", total_balance * price))
